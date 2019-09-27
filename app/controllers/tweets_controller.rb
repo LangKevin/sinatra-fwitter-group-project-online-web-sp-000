6d86d6f392e9c529a95d3f8e3d4ba72baef6470e
kevin lang <langkevina@gmail.com>
2019-09-27 00:04:57 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -66,6 +66,7 @@ class TweetsController < ApplicationController
         else
           @tweet = Tweet.find_by_id(params[:id])
           if @tweet && @tweet.user == current_user
+            
             if @tweet.update(content: params[:content])
               redirect to "/tweets/#{@tweet.id}"
             else
